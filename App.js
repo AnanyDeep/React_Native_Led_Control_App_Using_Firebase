16a7cf035c03897cf813fb9f7aacbc1036b6a4b1
Anany Deep <ananydeep2000@gmail.com>
2021-04-08 21:42:53 +0530

    UPDATED

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import firebase from 'firebase';
-import Firebase from './screens/Firebase';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Button, TouchableOpacity, Text } from "react-native";
+import firebase from "firebase";
+import Firebase from "./screens/Firebase";
 
 export default function App() {
+  const [status, setStatus] = useState([0]);
+  const [Hum, setHum] = useState(0);
 
-  useEffect(()=>{
-    const myitems = firebase.database().ref();
-    myitems.on("value",datasnap => {
+  // const [temp, setTemp] = useState([0]);
+
+  useEffect(() => {
+    const myitems = firebase.database().ref("Humidity");
+    myitems.on("value", (datasnap) => {
+      setStatus(Object.values(datasnap.val()));
+      setHum(status[0]);
+      // setTemp(Object.values(datasnap.val()));
       console.log(Object.values(datasnap.val()));
-    })
-  })
+      // console.log(status);
+    });
+  });
 
   const plus = () => {
     firebase.database().ref().update({
-      LED_STATUS : 1
+      LED_STATUS: 1,
     });
-  }
+  };
 
-  const minus  = () => {
+  const minus = () => {
     firebase.database().ref().update({
-      LED_STATUS : 0
+      LED_STATUS: 0,
     });
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Button title="Up" onPress={()=>{plus()}}  />
-      <Button title="Down" onPress={()=>{minus()}}  />
+      {!status ? (
+        <Text>No data</Text>
+      ) : (
+        <View>
+          {/* <Text>{status[0]}</Text> */}
+          <Text>{Hum}</Text>
+          {/* <Text>{temp[0]}</Text> */}
+        </View>
+      )}
+      <View style={styles.btn}>
+        <TouchableOpacity onPress={() => plus()}>
+          <Text style={styles.txt}>HIGH</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.btn}>
+        <TouchableOpacity onPress={() => minus()}>
+          <Text style={styles.txt}>LOW</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -38,8 +62,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "powderblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    marginTop: 50,
+  },
+  txt: {
+    fontSize: 50,
+    backgroundColor: "orange",
+    paddingHorizontal: 100,
+    borderRadius: 50,
+    color: "#FFF",
   },
 });
--- screens/Firebase.js
+++ screens/Firebase.js
@@ -1,17 +1,39 @@
-import firebase from 'firebase' 
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDxu0YWvepkPK8wPPKbdj7qHOv60OVYAAM",
-    authDomain: "testing-api-6758b.firebaseapp.com",
-    databaseURL: "https://testing-api-6758b-default-rtdb.firebaseio.com",
-    projectId: "testing-api-6758b",
-    storageBucket: "testing-api-6758b.appspot.com",
-    messagingSenderId: "702306414400",
-    appId: "1:702306414400:web:640577585b04b70d44d4a5",
-    measurementId: "G-HWTD5M6WRT"
-  };
+  apiKey: "AIzaSyDxu0YWvepkPK8wPPKbdj7qHOv60OVYAAM",
+  authDomain: "testing-api-6758b.firebaseapp.com",
+  databaseURL: "https://testing-api-6758b-default-rtdb.firebaseio.com",
+  projectId: "testing-api-6758b",
+  storageBucket: "testing-api-6758b.appspot.com",
+  messagingSenderId: "702306414400",
+  appId: "1:702306414400:web:640577585b04b70d44d4a5",
+  measurementId: "G-HWTD5M6WRT",
+};
 
-if(firebase.apps.length === 0){
-  firebase.initializeApp(firebaseConfig)
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDxu0YWvepkPK8wPPKbdj7qHOv60OVYAAM",
+//   authDomain: "testing-api-6758b.firebaseapp.com",
+//   databaseURL: "https://testing-api-6758b-default-rtdb.firebaseio.com",
+//   projectId: "testing-api-6758b",
+//   storageBucket: "testing-api-6758b.appspot.com",
+//   messagingSenderId: "702306414400",
+//   appId: "1:702306414400:web:640577585b04b70d44d4a5",
+//   measurementId: "G-HWTD5M6WRT"
+// };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB2nKtohksyRb6SYEl85lIAi0f6iNi8eRg",
+//   authDomain: "iot-t-h-4e490.firebaseapp.com",
+//   databaseURL: "https://iot-t-h-4e490-default-rtdb.firebaseio.com",
+//   projectId: "iot-t-h-4e490",
+//   storageBucket: "iot-t-h-4e490.appspot.com",
+//   messagingSenderId: "238958511632",
+//   appId: "1:238958511632:web:0f44750732dd81a6d8ec95",
+//   measurementId: "G-WGXVYC8DJL",
+// };
